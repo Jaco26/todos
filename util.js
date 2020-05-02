fb7a6f2731d277db58905e26963d40dcf3357520
Jacob Albright <jacob.albright23@gmail.com>
2020-05-02 17:26:00 -0500

    merged branch [dev].removed unused code

--- util.js
+++ util.js
@@ -1,67 +1,3 @@
-const componentFactory = (function() {
-
-  return {
-    makeCheckbox: function(text, isChecked) {
-      return {
-        tag: 'div',
-        data: {
-          class: 'md-checkbox'
-        },
-        children: [
-          {
-            tag: 'input',
-            data: {
-              attrs: {
-                checked: isChecked,
-                type: 'checkbox',
-              },
-            },
-          },
-          {
-            tag: 'span',
-            data: {
-              class: 'md-checkbox__label'
-            },
-            children: [text]
-          }
-        ]
-      }
-    },
-
-    makeHeading: function(text) {
-      return {
-        tag: 'h4',
-        children: [text.slice(1).trim()]
-      }
-    },
-
-    makePara: function(text) {
-      return {
-        tag: 'p',
-        children: [text.trim()]
-      }
-    },
-
-    makeItalics: function(text) {
-      return {
-        tag: 'em',
-        children: [text.replace(/\*/g, '')]
-      }
-    },
-
-    makeBold: function(text) {
-      return {
-        tag: 'strong',
-        children: [text.replace(/\*{2}/g, '')]
-      }
-    },
-
-
-
-  }
-
-})()
-
 
 function uuid() {
   const segmentLengths = [8, 4, 4, 4, 12]
@@ -75,14 +11,4 @@ function uuid() {
     segments.push(segment)
   }
   return segments.join('-')
-}
-
-function renderRecursive(createElement, node) {
-  if (typeof node === 'string') {
-    return node
-  }
-  const tag = node.tag
-  const data = Object.assign({}, node.data)
-  const children = (node.children || []).map(child => renderRecursive(createElement, child))
-  return createElement(tag, data, children)
 }
